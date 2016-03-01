68427f75188b5ac00bc0973589c55670f69e0784
Richard Silverton <silverton.richard@googlemail.com>
2016-03-01 22:07:22 +0000

    Uses Github API directly to collect project pages and contents

--- server/imports/github-interface.js
+++ server/imports/github-interface.js
@@ -1,19 +1,67 @@
-import GitHubApi from 'github'
+var baseUrl = 'https://api.github.com'
 
 export default function (userId) {
   var user = Meteor.users.findOne(userId)
   if (!user) throw new Meteor.Error('You must supply a valid userId')
   if (!user.services || !user.services.github || !user.services.github.accessToken) throw new Meteor.Error('User has no Github auth credentials')
+  var token = user.services.github.accessToken
 
-  var github = new GitHubApi({
-      version: '3.0.0',
-      protocol: 'https',
-      debug: true
-  })
-  github.authenticate({
-      type: 'oauth',
-      token: user.services.github.accessToken
-  })
+  function githubCall ({ method, url, opts }, cb) {
+    opts = opts || {}
+    opts.headers = opts.headers || {}
+    opts.headers.Authorization = `token ${token}`
+    opts.headers.Accept = opts.headers.Accept || 'application/vnd.github.v3+json'
+    opts.headers['User-Agent'] = Meteor.settings.appName
+    console.log(opts)
+    if (cb) return HTTP.call(method, url, opts, cb)
+    return HTTP.call(method, url, opts)
+  }
+
+  var github = {
+    getRepos: function (cb) {
+      var cb = cb ? makeReturnData('data', cb) : null
+      var res = githubCall({
+        method: 'GET',
+        url: `${baseUrl}/user/repos`,
+        opts: {
+          params: {
+            per_page: 100
+          }
+        }
+      }, cb)
+      if (!cb) return res.data
+    },
+
+    getDirContents: function (fullName, path, cb) {
+      var cb = cb ? makeReturnData('data', cb) : null
+      var res = githubCall({
+        method: 'GET',
+        url: `${baseUrl}/repos/${fullName}/contents/${path}`
+      }, cb)
+      if (!cb) return res.data
+    },
+
+    getFileContents: function (fullName, path, cb) {
+      var cb = cb ? makeReturnData('content', cb) : null
+      var res = githubCall({
+        method: 'GET',
+        url: `${baseUrl}/repos/${fullName}/contents/${path}`,
+        opts: {
+          headers: {
+            Accept: 'application/vnd.github.v3.raw'
+          }
+        }
+      }, cb)
+      if (!cb) return res.data
+    }
+  }
 
   return github
+}
+
+function makeReturnData (key, cb) {
+  return (err, res) => {
+    if (res) return cb(err, res[key])
+    cb(err)
+  }
 }
--- server/methods/github.js
+++ server/methods/github.js
@@ -1,9 +1,51 @@
+import async from 'async'
 import githubInterface from '../imports/github-interface'
 
 Meteor.methods({
   'github/getRepos' () {
     if (!this.userId) throw new Meteor.Error('Only a logged in user can retrieve their projects')
     var github = githubInterface(this.userId)
-    console.log(github)
+    return github.getRepos()
+            .map(({ name, full_name, created_at, updated_at }) => ({ name, full_name, created_at, updated_at }))
+  },
+  'github/getPages' (fullName) {
+    if (!this.userId) throw new Meteor.Error('Only a logged in user can retrieve their projects')
+    var github = githubInterface(this.userId)
+    return new Promise((resolve, reject) => {
+      getPages(github, fullName, (err, res) => {
+        if (err) return reject(err)
+        resolve(res)
+      })
+    }).catch(err => {
+      console.log(err)
+      throw new Meteor.Error(err.reason || err.error || 'Could not get pages')
+    })
   }
 })
+
+function getPages (github, fullName, cb) {
+  var contents = github.getDirContents(fullName, 'pages')
+  var pages = contents.filter(obj => obj.type === 'dir').map(obj => obj.name)
+  async.map(pages, (page, cb) => {
+    getPageContents(github, fullName, page, cb)
+  }, cb)
+}
+
+function getPageContents (github, fullName, page, cb) {
+  async.waterfall([
+    function getPageDirContents (cb) {
+      github.getDirContents(fullName, `pages/${page}`, cb)
+    },
+    function getPageDataJson (res, cb) {
+      var content = res.find(obj => obj.type === 'file' && obj.name === 'content.json')
+      if (!content) return cb('Missing content.json')
+      github.getFileContents(fullName, `pages/${page}/${content.name}`, cb)
+    }
+  ], (err, res) => {
+    if (err) return cb(err)
+    cb(null, {
+      name: page,
+      contents: JSON.parse(res)
+    })
+  })
+}
