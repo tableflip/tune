b80d36acf190a3462b081e407e3ee717f8b58752
Richard Silverton <silverton.richard@googlemail.com>
2016-03-08 07:50:46 +0000

    Removes some logging

--- server/publications/pages.js
+++ server/publications/pages.js
@@ -1,13 +1,9 @@
-var counter = 0
-
 Meteor.publish('page', function (pageId) {
-  console.log(`Counter is at ${counter++}`)
   var page = Pages.findOne(pageId)
   if (!page) return this.ready()
   var project = Projects.findOne(page.project._id)
   if (!project) return this.ready()
   if (project.users.indexOf(this.userId) < 0) return this.ready()
-  console.log('subscribing', page, pageId)
   return [
     Projects.find({ _id: page.project._id }),
     Pages.find(pageId)
