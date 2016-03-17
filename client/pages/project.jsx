d280b6d5dad55a1c81fb5cec62b03f37ce5e30ba
Alan Shaw <alan@tableflip.io>
2016-03-17 11:20:18 +0000

    Add minimal UI for publishing changes to LIVE

--- client/pages/project.jsx
+++ client/pages/project.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import Breadcrumbs from '../components/breadcrumbs'
+import OverlayLoader from '../components/overlay-loader'
 
 const Project = React.createClass({
   mixins: [ReactMeteorData],
@@ -11,6 +12,10 @@ const Project = React.createClass({
       project: Projects.findOne({ _id: this.props.projectId }),
       pages: Pages.find({ 'project._id': this.props.projectId }).fetch()
     }
+  },
+
+  getInitialState () {
+    return { publishing: false }
   },
 
   render () {
@@ -24,13 +29,36 @@ const Project = React.createClass({
         <div className="container">
           <h3 className="m-y-1 text-muted">{this.data.project.name}</h3>
           <p className="lead m-t-1">Pick a page</p>
-          <div className='list-group'>
+          <div className='list-group m-y-1'>
             <a className='list-group-item' href={`/project/${this.props.projectId}/facts`}>Website settings</a>
             {this.data.pages.map(page => (<a className='list-group-item' key={page._id} href={`/project/${this.data.project._id}/page/${page._id}`}>Page - {page.name}</a>))}
           </div>
+          <button type="button" className="btn btn-danger-outline btn-block" onClick={this.onPublishClick}>Publish</button>
         </div>
+        <OverlayLoader loaded={!this.state.publishing} />
       </div>
     )
+  },
+
+  onPublishClick (e) {
+    e.preventDefault()
+
+    // TODO: improve?
+    if (!window.confirm('Publish changes to your LIVE website?')) return
+
+    this.setState({ publishing: true })
+
+    Meteor.call('projects/tag', this.props.projectId, (err) => {
+      this.setState({ publishing: false })
+
+      if (err) {
+        // TODO: toastr error?
+        window.alert('Failed to publish changes please try again')
+        return console.error('Failed to tag', err)
+      }
+
+      // TODO: toastr success?
+    })
   }
 })
 
