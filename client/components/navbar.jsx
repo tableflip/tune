7b39a457296d89b6eda47488d6b7df34ae884641
Richard Silverton <silverton.richard@googlemail.com>
2016-04-07 18:14:19 +0100

    Puts correct link in navbar

--- client/components/navbar.jsx
+++ client/components/navbar.jsx
@@ -6,13 +6,14 @@ import { Icon } from './icon'
 
 const Navbar = React.createClass({
   render () {
+    let path = this.props.project && FlowRouter.path('project', { projectId: this.props.project._id })
     return (
       <div>
         <div className="navbar navbar-full navbar-dark bg-inverse">
           <ul className="nav navbar-nav">
             {this.props.project && (
               <li className="nav-item active pull-xs-left">
-                <a className="nav-link" href="#">{this.props.project.name}</a>
+                <a className="nav-link" href={path}>{this.props.project.name}</a>
               </li>
             )}
             <li className="nav-item pull-xs-right">
